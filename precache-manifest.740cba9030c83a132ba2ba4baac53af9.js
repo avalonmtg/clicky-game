735ae7bc7b1c6e5b208d73df73c2ee4afd34480d
JoanStoddard <joanstoddard16@gmail.com>
2020-06-22 11:18:31 -0700

    Updates

---
+++ precache-manifest.740cba9030c83a132ba2ba4baac53af9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6eae957a728c79b895053a89c3c8352b",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "c84d4cdfcfd1b79ce570",
+    "url": "/clicky-game/static/css/main.262c7abc.chunk.css"
+  },
+  {
+    "revision": "77ec5a0aa6834a00ede5",
+    "url": "/clicky-game/static/js/2.69b5d891.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/clicky-game/static/js/2.69b5d891.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c84d4cdfcfd1b79ce570",
+    "url": "/clicky-game/static/js/main.66fafcec.chunk.js"
+  },
+  {
+    "revision": "a2dd66fdceb11b57b463",
+    "url": "/clicky-game/static/js/runtime-main.3c5f3143.js"
+  }
+]);
